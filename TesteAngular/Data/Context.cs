f63101405c5bd09d783976eca23a12a474da8ff0
Victor <victor-hrj@hotmail.com>
2022-12-04 19:53:06 +0000

    Inserindo classe e data

---
+++ TesteAngular/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TesteAngular.Models;
+
+namespace TesteAngular.Data
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Contry {get;set;}
+    }
+}
---
+++ TesteAngular/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace TesteAngular.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public float VAT { get; set; }
+        public float Tax { get; set; }
+
+    }
+}
